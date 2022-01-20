926ffa5ccb12f1fc52cc50f3a39585b8378b8e46
Noaha811 <nanderson95@students.mchenry.edu>
2022-01-19 19:59:51 -0600

    Initial commit

---
+++ src/main.kt
@@ -0,0 +1,7 @@
+fun main(){
+    println("My name is Noah Anderson")
+    println("I enjoy programming, and have been doing it since I was 14 years old")
+    println("I have a dog named Harlee, she is an American Bulldog and is 4 years old")
+    println("I also enjoy cooking and video games")
+    println("I look forward to this class!")
+}
